cd1727cfb2fc4407e84eaf8b34c43d315095e1d8
asantex <agyei22@mail.com>
2025-03-27 21:16:47 +0000

    work section added

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 //java script for scrolling jobs
 
 function scrollLeft() {
-    const wrapper = document.querySelector('.image-wrap');
-    wrapper.scrollBy({
-        left: -100,//scroll distance adjustment
-        behavior: 'smooth'
-    });
-}
+        const wrapper = document.querySelector('.image-wrap');
+        wrapper.scrollBy({
+            left: -100,//scroll distance adjustment
+            behavior: 'smooth'
+        });
+    }
 function scrollRight() {
-    const wrapper = document.querySelector('.image-wrap');
-    wrapper.scrollBy({
-        left:100,
-        behavior: 'smooth'
+        const wrapper = document.querySelector('.image-wrap');
+        wrapper.scrollBy({
+            left: 100,
+            behavior: 'smooth'
+        });
+    }
+
+
+    const reveals = document.querySelectorAll('.reveal');
+
+    const observer = new IntersectionObserver((entries) =>{
+        entries.forEach((entry) =>{
+            if(entry.isIntersecting){
+                entry.target.classList.add('active');
+                observer.unobserve(entry.target);
+            }
+        });
+    },{threshold:0.1});
+
+    reveals.forEach((reveal) =>{
+        observer.observe(reveal);
     });
-}
+
+
+    feather.replace()
